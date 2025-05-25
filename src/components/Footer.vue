d993ade4617750f9b1f93aff1ac1a91d560de8b5
Alycia <qw159321@gmail.com>
2025-05-25 22:53:24 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="fixed bottom-0 bg-slate-900 opacity-90 w-full text-white font-bold flex  p-8 text-lg">
+        <div class="mr-10 w-[15%]">
+            <p class="mb-4">關於網站</p>
+            <hr>
+            <ul class="mt-4">
+                <li>此網站僅用為專案練習</li>
+                <li>不作為商業用途使用</li>
+            </ul>
+        </div>
+        <div class="w-[15%]">
+            <p class="mb-4">資料來源</p>
+            <hr>
+            <ul class="mt-4">
+                <li>KKBOX</li>
+                <li>YouTube</li>
+            </ul>
+        </div>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col md:flex-row justify-between items-center p-8 font-bold bg-black bg-opacity-85 sticky top-0">
+        
+            <h1 class="text-3xl text-white mb-3 md:mb-0">
+                <RouterLink to="/">
+                音樂榜
+                </RouterLink>
+            </h1>
+        <div class=" flex flex-col md:flex-row items-center justify-between ">
+            <input class="w-[74%] md:mr-3 mb-3 md:mb-0 rounded-md p-2 text-zinc-800" type="search" placeholder="請輸入歌名">
+            <button class="text-lg w-[24%] text-white border rounded-md hover:bg-slate-50 hover:text-slate-900 p-1" type="button">搜尋</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { RouterLink } from 'vue-router'
+
+</script>
