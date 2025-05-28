f6e1e98d046bfa5a37dd963936ded762f5d37fb6
“jiafen” <qw159321@wisenvr.com>
2025-05-28 11:32:29 +0800

    調整環境變數+base路徑+github workflows+新增頁面

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="fixed bottom-0 bg-slate-900 opacity-90 w-full text-white font-bold flex  p-8 text-lg">
+    <div class="bg-slate-900 opacity-90 w-full text-white font-bold flex px-12 py-6 text-lg">
         <div class="mr-10 w-[15%]">
             <p class="mb-4">關於網站</p>
             <hr>
-            <ul class="mt-4">
+            <ul class="mt-4 text-sm">
                 <li>此網站僅用為專案練習</li>
                 <li>不作為商業用途使用</li>
             </ul>
@@ -11,7 +11,7 @@
         <div class="w-[15%]">
             <p class="mb-4">資料來源</p>
             <hr>
-            <ul class="mt-4">
+            <ul class="mt-4 text-sm">
                 <li>KKBOX</li>
                 <li>YouTube</li>
             </ul>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,125 @@
 <script setup>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
+  <div class="bg-black">
     <Header />
-    
-    <Footer /> 
+    <header>
+      <div
+        class="relative flex flex-col items-center justify-center text-center"
+      >
+        <img
+          @load="handleImg"
+          class="h-[calc(100vh-70px)] w-full object-cover object-center"
+          src="https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+        />
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div class="absolute bottom-52 text-white">
+          <h1 class="text-6xl font-bold mb-4 leading-relaxed opacity-80">
+            <p class="pr-20">來音樂的世界</p>
+            <p class="pl-20">聽世界的音樂</p>
+          </h1>
+        </div>
+      </div>
+    </header>
+    <main class="mt-10 bg-slate-950 px-12">
+      <section class="mx-auto text-center text-white mb-10">
+        <h2 class="mb-5 font-bold text-white text-center text-3xl md:text-4xl">
+          一起享受世界的聲音
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+          <div>
+            <span class="material-symbols-outlined text-7xl"> queue_music </span>
+            <div class="bg-gray-800 hover:bg-gray-700 h-full rounded-md">
+              <div class="p-4 bg-hover">
+                <h3 class="mb-3 text-xl font-bold">即時新歌</h3>
+                <p>提供最新的歌曲<br />中英日韓，任您挑選</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span class="material-symbols-outlined text-7xl"> youtube_activity </span>
+            <div class="bg-gray-800 hover:bg-gray-700 h-full rounded-md">
+              <div class="p-4 bg-hover">
+                <h3 class="mb-3 text-xl font-bold">串接YouTube</h3>
+                <p>除了聲音上的享受<br />也滿足您的視覺</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span class="material-symbols-outlined text-7xl"> favorite </span>
+            <div class="bg-gray-800 hover:bg-gray-700 h-full rounded-md">
+              <div class="p-4 bg-hover">
+                <h3 class="mb-3 text-xl font-bold">情境歌單</h3>
+                <p>挑選符合您<br />此刻的心情的歌單吧！</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="container mx-auto">
+        <div class="mb-5">
+          <div
+            class="text-center flex flex-col sm:flex-row items-center justify-center mb-3"
+          >
+            <p
+              class="text-center text-white text-3xl md:text-4xl font-bold mb-3 sm:mb-0 sm:mr-5"
+            >
+              新歌即時榜 TOP 4
+            </p>
+            <a
+              class="no-underline whitespace-nowrap font-bold border border-white text-white py-2 px-4 rounded"
+              href="./pages/charts.html"
+              >完整榜單</a
+            >
+          </div>
+          <div
+            class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center"
+            data-chartSongs="chartSongs"
+          >
+            <div class="text-center">
+              <div class="spinner-border text-light" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-5">
+          <p class="text-white text-center text-3xl md:text-4xl font-bold mb-3">
+            速爆新歌
+          </p>
+          <div
+            class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center"
+            data-hitsSongs="hitsSongs"
+          >
+            <div class="text-center">
+              <div class="spinner-border text-light" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p class="text-white text-center text-3xl md:text-4xl font-bold mb-3">
+            情境歌單
+          </p>
+          <div
+            class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center"
+            data-sessionSongs="sessionSongs"
+          >
+            <div class="text-center">
+              <div class="spinner-border text-light" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <Footer />
+  </div>
 </template>
