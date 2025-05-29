e040c227f5359e65c4db518ad5a317c6bded2b5b
“jiafen” <qw159321@wisenvr.com>
2025-05-29 16:56:50 +0800

    新增api

--- src/composables/songsListApi.js
+++ src/composables/songsListApi.js
@@ -1,14 +1,34 @@
-import { GET, POST, DELETE } from '@/composables/base';
+import { GET, POST, DELETE } from "@/composables/base";
 
 const getToken = () => {
-    const res = GET(`/api/token`);
-    return res;
+  const res = GET(`/api/token`);
+  return res;
 };
 
 const getNewSongs = () => {
-    const res = GET(`/api/kkbox/chart`);
-    return res;
+  const res = GET(`/api/kkbox/chart`);
+  return res;
 };
 
+const getNewHitSongs = () => {
+  const res = GET(`/api/kkbox/new-hits`);
+  return res;
+};
 
-export { getToken, getNewSongs }
+const getSessionsSongs = () => {
+  const res = GET(`/api/kkbox/sessions`);
+  return res;
+};
+
+const getHitsTracks = async (id) => {
+  const res = GET(`/api/kkbox/new-hits/${id}`);
+  return res;
+};
+
+export {
+  getToken,
+  getNewSongs,
+  getNewHitSongs,
+  getSessionsSongs,
+  getHitsTracks,
+};
